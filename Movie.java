7b5b34c6356206698719a98d2f2e6ee878e518c2
nhaggerty <43151650+nhaggerty@users.noreply.github.com>
2018-12-06 19:49:12 -0500

    Step 10

--- Movie.java
+++ Movie.java
@@ -45,5 +45,12 @@ public class Movie {
         }
         return newAmount;
     }
+    
+    public int getFrequentRenterPoints(int daysRented){
+        if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
+            return 2;
+        else
+            return 1;
+    }
 
 }
